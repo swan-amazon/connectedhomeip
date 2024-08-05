57370dd68fc1c66005035756ebb76f15416f2104
James Swan <122404367+swan-amazon@users.noreply.github.com>
2024-08-05 18:22:21 +0000

    wip

--- examples/all-clusters-app/linux/include/CHIPProjectAppConfig.h
+++ examples/all-clusters-app/linux/include/CHIPProjectAppConfig.h
@@ -51,3 +51,7 @@
 // Claim a device type while advertising that matches the device type on our
 // endpoint 1.  0x0101 is the "Dimmable Light" device type.
 #define CHIP_DEVICE_CONFIG_DEVICE_TYPE 0x0101
+
+#define CHIP_CONFIG_TC_REQUIRED (1)
+#define CHIP_CONFIG_TC_REQUIRED_ACKNOWLEDGEMENTS 1
+#define CHIP_CONFIG_TC_REQUIRED_ACKNOWLEDGEMENTS_VERSION 1
